f79c36a3a58df38af19774a46dfd20bc1cd23b83
jinxscript <omolemotswaolesele@gmail.com>
2024-03-19 20:37:30 +0200

    Repetition

---
+++ primarilitytest/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+
+/*
+
+bool isPrime(int n){
+
+    if(n == 1){
+        return false;
+        for (int i = 2; i < n; i++){
+
+            if(n % i == 0){
+                return false
+}
+        }
+        return true
+
+*/
