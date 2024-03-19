0eb39d213ca61435eba297288775b2b621a5721f
jinxscript <omolemotswaolesele@gmail.com>
2024-03-19 16:50:36 +0200

    GCD AND LCM

---
+++ ehabandgcd/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,t;
+    cin >>t;
+    for(int i = 0; i < t; i++){
+        cin >> x;
+        cout << 1<< " " << x-1 << endl;
+    }
+
+    return 0;
+}
